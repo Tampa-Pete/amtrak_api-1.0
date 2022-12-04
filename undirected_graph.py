6f385ada719ecf3a2bd3d1c17ec2b2130db3fad1
Tampa-Pete <53914959+Tampa-Pete@users.noreply.github.com>
2022-12-04 02:58:50 -0500

    Create undirected_graph.py
    adding connections for tree with fewest nodes

--- undirected_graph.py
+++ undirected_graph.py
@@ -5,3 +5,93 @@ class udg:
         self.help()
     def help(self):
         print("USAGE")
+
+# for connection.py file:
+# Uses  a database of stations, trains, and schedules to map the best route
+# for an Amtrack travel.
+
+#pseudocode
+startstation = 'Chicago'
+endstation = 'Miami'
+routes = [
+    'Empire Builder',
+    'California Zephyr',
+    'Coast Starflight',
+    'Sunset Limited',
+    'City of New Orleans',
+    'Lake Shore Limited',
+    'Crescent',
+    'Cardinal',
+    'Silver Star',
+    'Regional'
+]
+#station list with available routes
+station_routes = {
+    'Chicago' : (
+        'Empire Builder',
+        'Califoria Zephyr',
+        'City of New Orleans',
+        'Lake Shore Limited',
+        'Cardinal'
+    ),
+    'San Francisco' : (
+        'Califoria Zephyr',
+        #'Coast Starflight' #will add when edges better implemented
+    ),
+    'Seattle' : (
+        'Empire Builder',
+        'Coast Starflight'
+    ),
+    'Los Angles' : (
+        'Coast Starflight',
+        'Sunset Limited'
+    ),
+    'New Orleans' : (
+        'Sunset Limited',
+        'City of New Orleans',
+        'Crescent'
+    ),
+    'New York' : (
+        'Lake Shore Limited',
+        'Crescent',
+        'Silver Star',
+        #'Cardinal',    #will add when edges better implemented
+        'Regional'
+    ),
+    'Washington, D.C.' : (
+        'Cardinal',
+        'Regional',
+        #'Silver Star'  #will add when edges better implemented
+    ),
+    'Miami' : (
+        'Silver Star'
+    )
+}
+
+#build the graph from edges connecting verticies
+G = {}  # dict of tuple keys for route list
+for s in station_routes:
+    for r in station_routes[s]:
+        #search tree with competing edge
+        for search in station_routes:
+            if r in station_routes[search] and s != search:
+                if G[(s,search)] is null:
+                    G[(s,search)] = [r]
+                else:
+                    G[(s,search)].append(r)
+                # print(f"found matching {r} between {s} and {search}")
+print(*G, sep=', ')
+
+#create a tree from the graph by removing cycles
+# undirected graph
+# start with a dictionary of listed connections G
+# get list of all possible routes H, where a route h is a list of nodes
+query = start
+connections = G[query] #value is list
+# create tree where only one node exists
+
+for (i,j) in keys(G):
+    try:
+        G.pop(j,i)
+    except KeyError:
+        continue
